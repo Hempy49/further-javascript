5920b3d183c3ba5a026343d3921558d543be5bf9
Simon Hempenstall <simonjhemp@hotmail.com>
2017-07-20 15:12:53 +0100

    Limit notes to 20 chars

--- spec/note-list-view-spec.js
+++ spec/note-list-view-spec.js
@@ -21,3 +21,11 @@ it("test to Html no note models", function () {
 it("test to Html for each text", function () {
   assert.isTrue(noteListView.toHtmlforEachText() === "<li><div>Note message</div></li>")
 });
+
+it("test note longer than 20 chars", function () {
+  var longNote = new Note("123456789123456789123");
+  var noteList3 = new NoteList();
+  noteList3.pushToNoteModels(longNote);
+  var noteListView3 = new NoteListView(noteList3);
+  assert.isTrue(noteListView3.toHtml() === "<ul><li><div>12345678912345678912</div></li></ul>")
+});
