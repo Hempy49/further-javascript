423c9f8c5bb8c003106f93f9bc2dc241faf6113a
Simon Hempenstall <simonjhemp@hotmail.com>
2017-07-21 16:07:27 +0100

    Spike submit event

--- controllers/index.js
+++ controllers/index.js
@@ -4,34 +4,3 @@ var noteList = new NoteList()
 noteList.pushToNoteModels(newNote)
 noteList.pushToNoteModels(newNote2)
 var noteController = new NoteController(noteList)
-
-window.onload = function() {
-  var element = document.getElementById("app")
-  noteController.getHtml(element)
-}
-makeNewURLForNoteID();
-
-function makeNewURLForNoteID() {
-  window.addEventListener("hashchange", showSingleNote);
-};
-
-function showSingleNote() {
-  showNote(getNoteID(window.location));
-};
-
-function getNoteID(location) {
-  return location.hash.split("#")[1];
-}
-
-function showNote(id) {
-  var noteText;
-  noteList.getNotes().forEach(function(note) {
-    if (note.id == id) {
-      noteText = note.text;
-    }
-  });
-
-  document
-  .getElementById("app")
-  .innerHTML = noteText;
-};
--- controllers/note-controller.js
+++ controllers/note-controller.js
@@ -3,10 +3,50 @@
     this.noteList = noteList;
     this.noteListView = new NoteListView(this.noteList);
   }
-  
+
   NoteController.prototype.getHtml = function (element) {
     element.innerHTML = this.noteListView.toHtml();
   };
-  
+
+  window.onload = function() {
+    var element = document.getElementById("app");
+    noteController.getHtml(element)
+    var ele = document.getElementById("text");
+    ele.addEventListener("submit",
+    function (test) {
+      test.preventDefault();
+      console.log(test);
+      return false;
+    }, false);
+  };
+
+
+  makeNewURLForNoteID();
+
+  function makeNewURLForNoteID() {
+    window.addEventListener("hashchange", showSingleNote);
+  };
+
+  function showSingleNote() {
+    showNote(getNoteID(window.location));
+  };
+
+  function getNoteID(location) {
+    return location.hash.split("#")[1];
+  }
+
+  function showNote(id) {
+    var noteText;
+    noteList.getNotes().forEach(function(note) {
+      if (note.id == id) {
+        noteText = note.text;
+      }
+    });
+
+    document
+    .getElementById("app")
+    .innerHTML = noteText;
+  };
+
   exports.NoteController = NoteController;
 })(this);
